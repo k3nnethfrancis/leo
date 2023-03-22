4a856f9092cc12a7516f60de4cb471ae36301778
Ken <kennycav93@gmail.com>
2023-03-21 20:53:46 -0700

    updated readme, model, commented shit
    updated readme, model now gpt3.5-turbo, commented shit with gpt4

--- src/completion.py
+++ src/completion.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules
 from enum import Enum
 from dataclasses import dataclass
 import openai
@@ -16,10 +17,11 @@ from src.moderation import (
     send_moderation_blocked_message,
 )
 
+# Define constants for bot name and example conversations
 MY_BOT_NAME = BOT_NAME
 MY_BOT_EXAMPLE_CONVOS = EXAMPLE_CONVOS
 
-
+# Define an enumeration for completion results
 class CompletionResult(Enum):
     OK = 0
     TOO_LONG = 1
@@ -28,7 +30,7 @@ class CompletionResult(Enum):
     MODERATION_FLAGGED = 4
     MODERATION_BLOCKED = 5
 
-
+# Define a data class for completion data, which stores the status, reply text, and status text
 @dataclass
 class CompletionData:
     status: CompletionResult
@@ -36,10 +38,12 @@ class CompletionData:
     status_text: Optional[str]
 
 
+# Asynchronous function to generate a response using GPT-3.5-turbo model
 async def generate_completion_response(
     messages: List[Message], user: str
 ) -> CompletionData:
     try:
+        # Create a Prompt object using the given messages and predefined bot instructions and example conversations
         prompt = Prompt(
             header=Message(
                 "System", f"Instructions for {MY_BOT_NAME}: {BOT_INSTRUCTIONS}"
@@ -47,20 +51,31 @@ async def generate_completion_response(
             examples=MY_BOT_EXAMPLE_CONVOS,
             convo=Conversation(messages + [Message(MY_BOT_NAME)]),
         )
+
+        # Render the prompt into a string to be used as input for the GPT model
         rendered = prompt.render()
+
+        # Generate a response using the OpenAI API with the rendered prompt
         response = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="gpt-3.5-turbo",
             prompt=rendered,
             temperature=1.0,
             top_p=0.9,
             max_tokens=512,
-            stop=["<|endoftext|>"],
+            stop=[""],
         )
+
+        # Extract and clean the text from the response
         reply = response.choices[0].text.strip()
+
+        # Check if a reply was generated
         if reply:
+            # Moderate the generated reply
             flagged_str, blocked_str = moderate_message(
                 message=(rendered + reply)[-500:], user=user
             )
+
+            # If the reply contains blocked content, return a moderation-blocked CompletionData
             if len(blocked_str) > 0:
                 return CompletionData(
                     status=CompletionResult.MODERATION_BLOCKED,
@@ -68,6 +83,7 @@ async def generate_completion_response(
                     status_text=f"from_response:{blocked_str}",
                 )
 
+            # If the reply contains flagged content, return a moderation-flagged CompletionData
             if len(flagged_str) > 0:
                 return CompletionData(
                     status=CompletionResult.MODERATION_FLAGGED,
@@ -75,36 +91,51 @@ async def generate_completion_response(
                     status_text=f"from_response:{flagged_str}",
                 )
 
+        # If the reply is valid, return a successful CompletionData object
         return CompletionData(
             status=CompletionResult.OK, reply_text=reply, status_text=None
         )
+
+    # Handle InvalidRequestError exceptions from the OpenAI API
     except openai.error.InvalidRequestError as e:
         if "This model's maximum context length" in e.user_message:
+            # If the input exceeds the model's maximum context length, return a too-long CompletionData
             return CompletionData(
                 status=CompletionResult.TOO_LONG, reply_text=None, status_text=str(e)
             )
         else:
+            # Log the exception and return an invalid-request CompletionData object
             logger.exception(e)
             return CompletionData(
                 status=CompletionResult.INVALID_REQUEST,
                 reply_text=None,
                 status_text=str(e),
             )
+
+    # Handle any other exceptions that might occur
     except Exception as e:
+        # Log the exception and return an other-error CompletionData object
         logger.exception(e)
         return CompletionData(
             status=CompletionResult.OTHER_ERROR, reply_text=None, status_text=str(e)
         )
 
 
+
+# Asynchronous function to process the completion response and send messages in the discord thread accordingly
 async def process_response(
     user: str, thread: discord.Thread, response_data: CompletionData
 ):
+    # Extract relevant information from the CompletionData object
     status = response_data.status
     reply_text = response_data.reply_text
     status_text = response_data.status_text
+
+    # Handle OK and Moderation Flagged responses
     if status is CompletionResult.OK or status is CompletionResult.MODERATION_FLAGGED:
         sent_message = None
+
+        # If the reply is empty, send an "Invalid response" message
         if not reply_text:
             sent_message = await thread.send(
                 embed=discord.Embed(
@@ -112,10 +143,13 @@ async def process_response(
                     color=discord.Color.yellow(),
                 )
             )
+        # If the reply is not empty, split it into shorter messages and send them
         else:
             shorter_response = split_into_shorter_messages(reply_text)
             for r in shorter_response:
                 sent_message = await thread.send(r)
+
+        # If the response is flagged by moderation, send a moderation flagged message
         if status is CompletionResult.MODERATION_FLAGGED:
             await send_moderation_flagged_message(
                 guild=thread.guild,
@@ -125,13 +159,16 @@ async def process_response(
                 url=sent_message.jump_url if sent_message else "no url",
             )
 
+            # Inform the user that the conversation has been flagged by moderation
             await thread.send(
                 embed=discord.Embed(
                     description=f"⚠️ **This conversation has been flagged by moderation.**",
                     color=discord.Color.yellow(),
                 )
             )
+    # Handle Moderation Blocked responses
     elif status is CompletionResult.MODERATION_BLOCKED:
+        # Send a moderation blocked message
         await send_moderation_blocked_message(
             guild=thread.guild,
             user=user,
@@ -139,22 +176,29 @@ async def process_response(
             message=reply_text,
         )
 
+        # Inform the user that the response has been blocked by moderation
         await thread.send(
             embed=discord.Embed(
                 description=f"❌ **The response has been blocked by moderation.**",
                 color=discord.Color.red(),
             )
         )
+    # Handle Too Long responses
     elif status is CompletionResult.TOO_LONG:
+        # Close the thread due to the response being too long
         await close_thread(thread)
+    # Handle Invalid Request responses
     elif status is CompletionResult.INVALID_REQUEST:
+        # Inform the user that the request was invalid
         await thread.send(
             embed=discord.Embed(
                 description=f"**Invalid request** - {status_text}",
                 color=discord.Color.yellow(),
             )
         )
+    # Handle any other errors
     else:
+        # Inform the user that an error occurred
         await thread.send(
             embed=discord.Embed(
                 description=f"**Error** - {status_text}",
--- src/constants.py
+++ src/constants.py
@@ -5,37 +5,42 @@ import yaml
 from typing import Dict, List
 from src.base import Config
 
-load_dotenv()
+# Load environment variables from the specified file
+load_dotenv('/home/jovyan/leo/botenv.env')
 
-
-# load config.yaml
+# Read and load the configuration file
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG: Config = dacite.from_dict(
     Config, yaml.safe_load(open(os.path.join(SCRIPT_DIR, "config.yaml"), "r"))
 )
 
+# Extract bot-related information from the configuration file
 BOT_NAME = CONFIG.name
 BOT_INSTRUCTIONS = CONFIG.instructions
 EXAMPLE_CONVOS = CONFIG.example_conversations
 
+# Get Discord and OpenAI API keys from environment variables
 DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 DISCORD_CLIENT_ID = os.environ["DISCORD_CLIENT_ID"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
+# Read and process the allowed server IDs from environment variables
 ALLOWED_SERVER_IDS: List[int] = []
 server_ids = os.environ["ALLOWED_SERVER_IDS"].split(",")
 for s in server_ids:
     ALLOWED_SERVER_IDS.append(int(s))
 
+# Read and process server-to-moderation channel mapping from environment variables
 SERVER_TO_MODERATION_CHANNEL: Dict[int, int] = {}
 server_channels = os.environ.get("SERVER_TO_MODERATION_CHANNEL", "").split(",")
 for s in server_channels:
     values = s.split(":")
     SERVER_TO_MODERATION_CHANNEL[int(values[0])] = int(values[1])
 
-# Send Messages, Create Public Threads, Send Messages in Threads, Manage Messages, Manage Threads, Read Message History, Use Slash Command
+# Generate the bot invite URL with required permissions
 BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=328565073920&scope=bot"
 
+# Set the moderation threshold values for blocked and flagged content
 MODERATION_VALUES_FOR_BLOCKED = {
     "hate": 0.5,
     "hate/threatening": 0.1,
@@ -56,12 +61,9 @@ MODERATION_VALUES_FOR_FLAGGED = {
     "violence/graphic": 0.1,
 }
 
-SECONDS_DELAY_RECEIVING_MSG = (
-    3  # give a delay for the bot to respond so it can catch multiple messages
-)
-MAX_THREAD_MESSAGES = 200
-ACTIVATE_THREAD_PREFX = "💬✅"
-INACTIVATE_THREAD_PREFIX = "💬❌"
-MAX_CHARS_PER_REPLY_MSG = (
-    1500  # discord has a 2k limit, we just break message into 1.5k
-)
+# Define constants related to bot behavior and messaging
+SECONDS_DELAY_RECEIVING_MSG = 3  # Delay to catch multiple messages
+MAX_THREAD_MESSAGES = 200  # Maximum number of messages in a thread
+ACTIVATE_THREAD_PREFX = "💬✅"  # Prefix for active threads
+INACTIVATE_THREAD_PREFIX = "💬❌"  # Prefix for inactive threads
+MAX_CHARS_PER_REPLY_MSG = 1500  # Maximum characters per reply message (Discord limit: 2000)
